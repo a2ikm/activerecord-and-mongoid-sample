2ad3395b9740192982f1c1fad009096ab5690762
Masato Ikeda <masato.ikeda@gmail.com>
2011-07-30 15:28:27 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiverecordAndMongoidSample::Application.config.secret_token = '7422778848b2e5dcbbab48ab06bb8a7345fceaa50a9c8854c432a507fbc1d2f9192e22248c70ec8d4369d99042a9fbb82ae02430c7a064422490e79d46e76333'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ActiverecordAndMongoidSample::Application.config.session_store :cookie_store, :key => '_activerecord-and-mongoid-sample_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ActiverecordAndMongoidSample::Application.config.session_store :active_record_store
