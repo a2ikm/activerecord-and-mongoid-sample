eddd492f85ae17582d95a7a8a0a489ee0c00e041
Masato Ikeda <masato.ikeda@gmail.com>
2011-07-30 18:22:05 +0900

    After creating or updating a Todo(ActiveRecord), app create TodoLog(Mongoid).

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,9 @@
 class Todo < ActiveRecord::Base
+  after_create :create_log
+  after_update :create_log
+  
+  private
+  def create_log
+    TodoLog.create(self.attributes.symbolize_keys.slice(:title, :done))
+  end
 end
